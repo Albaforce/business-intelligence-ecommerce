7bb43bd0dc1f4e4c94d4d15541ff4a227ef733d1
Nawel Bellil <153316848+Nawel-Bellil@users.noreply.github.com>
2025-01-12 12:31:46 +0100

    initial project setup

--- main.py
+++
@@ -1,9 +0,0 @@
-import scripts.etl as etl
-import scripts.analysis as analysis
-
-if __name__ == "__main__":
-    print("Starting ETL process...")
-    etl.clean_data()
-
-    print("Starting analysis...")
-    analysis.analyze_data()
